e36c85e767574545bd795401cf6ae418679bffdb
dumpdyno <118197098+dumpdyno@users.noreply.github.com>
2022-11-17 22:00:31 +0530

    Update cb_handler.py

--- plugins/cb_handler.py
+++ plugins/cb_handler.py
@@ -100,7 +100,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
     elif cb.data == "document":
         UPLOAD_AS_DOC.update({f"{cb.from_user.id}": True})
         await cb.message.edit(
-            text="Do you want to rename? Default file name is **[@yashoswalyo]_merged.mkv**",
+            text="Do you want to rename? Default file name is **[mr_technocrat]_merged.mkv**",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -164,7 +164,7 @@ async def callback_handler(c: Client, cb: CallbackQuery):
             return
         if "NO" in cb.data:
             new_file_name = (
-                f"downloads/{str(cb.from_user.id)}/[@yashoswalyo]_merged.mkv"
+                f"downloads/{str(cb.from_user.id)}/[mr_technocrat]_merged.mkv"
             )
             if user.merge_mode == 1:
                 await mergeNow(c, cb, new_file_name)
